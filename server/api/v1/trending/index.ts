5cc04f53b32e9822d6f73bdbde2d8fd7211f6014
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-07-14 21:06:39 +0200

    Improved typing of Hoyo requests. Temporarily removed error checking in trending endpoint.

--- server/api/v1/trending/index.ts
+++ server/api/v1/trending/index.ts
@@ -1,32 +1,26 @@
 import axios from 'axios';
 import { ApiError } from '~~/types/api';
 import { Topic } from '~~/types/topic';
+import { TrendResponse } from '~~/types/Hoyoverse/trending-response';
 
-export default defineEventHandler(async (event): Promise<Topic[] | ApiError> => {
+export default defineEventHandler(async (event): Promise<Topic[]> => {
     // https://bbs-api-os.hoyolab.com/community/painter/wapi/explore/topic/list
     // message: 'OK'
     // data->list: Array->base->[id, name, desc, cover]
     
     const endpoint: string = "https://bbs-api-os.hoyolab.com/community/painter/wapi/explore/topic/list";
-    const resp = await axios.get(endpoint);
+    const resp = await axios.get<TrendResponse>(endpoint);
     const data = await resp.data;
-
-    if(resp.status !== 200 || data.message !== "OK") {
-        const result: ApiError = {
-            code: data.retcode || resp.status,
-            msg: data.message || resp.statusText
-        }
-    }
 
     let result: Topic[] = [];
     data["data"]["list"].forEach(topic => {
         result.push({
-            id: topic.base.id,
+            id: parseInt(topic.base.id),
             name: topic.base.name,
             desc: topic.base.desc,
             stats: { 
-                views: topic.stat.view_num, 
-                replies: topic.stat.reply_num
+                views: parseInt(topic.stat.view_num), 
+                replies: parseInt(topic.stat.reply_num)
             }
         });
     });
