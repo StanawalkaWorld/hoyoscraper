8ddc17f60ff19bb6b333cb939f7a7d3fbdc6127c
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-07-21 08:39:15 +0200

    Modified Tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        `components/**/*.{vue,js}`,
+        `layouts/**/*.vue`,
+        `pages/**/*.vue`,
+        `composables/**/*.{js,ts}`,
+        `plugins/**/*.{js,ts}`,
+        `App.{js,ts,vue}`,
+        `app.{js,ts,vue}`,
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+
