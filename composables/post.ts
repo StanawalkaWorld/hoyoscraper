ec3d286d32b68e9270ffe72668b0f87013742d66
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-08-05 12:34:38 +0200

    Created post composable.

---
+++ composables/post.ts
@@ -0,0 +1,34 @@
+import { MaybeRef } from "~~/types/monad";
+import { useQuery } from "vue-query";
+import { Post } from "~~/types/post";
+
+export const usePost = (id: MaybeRef<number>) => {
+    return useQuery<Post>(
+        ["post", unref(id)],
+        () => {
+            return $fetch(`/api/v1/post/${id}`);
+        },
+        {
+            placeholderData: {
+                id: 0,
+                user: {
+                    avatar_url: "",
+                    id: 0,
+                    level: 0,
+                    nickname: "...",
+                },
+                content: "Loading...",
+                cover: "",
+                created_at: new Date(),
+                game: "Loading...",
+                is_official: false,
+                stats: {
+                    replies: 0,
+                    views: 0,
+                },
+                subject: "Loading...",
+                topics: [69],
+            },
+        }
+    );
+};
