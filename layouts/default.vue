9916c9321982e662105e489c5a9bf73cc013123f
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-08-05 10:13:41 +0200

    Added essential styling for modal img.

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,12 +17,11 @@ const imgState = useState("modal_img", () => "");
     <div
         v-if="imgState"
         class="fixed top-0 left-0 bottom-0 right-0 bg-black bg-opacity-50 flex justify-center items-center"
-        @click="imgState = ''"
+        @click.self="imgState = ''"
     >
         <div
             :style="{ backgroundImage: `url('${imgState}')` }"
-            class="w-full md:w-1/2 aspect-video"
-            @click.self
+            class="w-full md:w-1/2 aspect-video anyimg"
         ></div>
     </div>
 </template>
