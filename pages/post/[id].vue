cf2fe5f64e71dfbfaf5b6044defb9d6bd1cdc10f
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-08-05 13:31:24 +0200

    Created single post page.

---
+++ pages/post/[id].vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import PostRecord from "@/components/PostRecord.vue";
+import AlertBlock from "@/components/AlertBlock.vue";
+
+const route = useRoute();
+const { data, isError, error, isFetching } = usePost(
+    parseInt(route.params.id as string)
+);
+</script>
+
+<template>
+    <section class="flex flex-col items-center">
+        <PostRecord :post="data" class="container mx-auto mt-10" />
+
+        <AlertBlock :active="isFetching">Loading posts...</AlertBlock>
+        <AlertBlock danger :active="isError">
+            An error has occured while loading posts: {{ error }}
+        </AlertBlock>
+    </section>
+</template>
