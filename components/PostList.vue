25c2b062ec558e32d88d142f24c96a9c212e1beb
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-08-05 14:00:06 +0200

    Removed useless Transition.

--- components/PostList.vue
+++ components/PostList.vue
@@ -9,11 +9,9 @@ const props = defineProps<PostListProps>();
 </script>
 
 <template>
-    <Transition name="opacity" mode="in-out">
-        <div
-            class="bg-slate-800 lg:rounded-md lg:w-2/3 xl:w-1/3 border border-sky-900"
-        >
-            <PostRecord v-for="post in posts" :key="post.id" :post="post" />
-        </div>
-    </Transition>
+    <div
+        class="bg-slate-800 lg:rounded-md lg:w-2/3 xl:w-1/3 border border-sky-900"
+    >
+        <PostRecord v-for="post in posts" :key="post.id" :post="post" />
+    </div>
 </template>
